3637d7335d3a1630c147fc45885a6911cb76f2e2
Deepanshu0106 <beingdeepanshu2@gmail.com>
2026-03-30 00:49:03 +0530

    Data Path Problem Addressed

--- database.py
+++ database.py
@@ -1,4 +1,7 @@
 from supabase import create_client, Client
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import settings
 
 class DatabaseManager:
